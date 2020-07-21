a6721490decac64d78445b4cebf4e17ff1893d23
JoHannah Joy <jojojoy2012@gmail.com>
2020-07-20 21:47:53 -0700

    form submitting to individual pages

--- birdie_app/urls.py
+++ birdie_app/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
 from django.conf.urls import include, url
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
 from . import views
 
 app_name = 'birdie_app'
 urlpatterns = [
     # path('/', views.home, name='home'),
     path('data/', views.data, name='data'),
-    # how to make data transfer to personal html
-    path('bryan/', views.BryanView.as_view(template_name='bryan.html'), name='bryan'),
-    path('david/', views.DavidView.as_view(template_name='david.html'), name='david'),
-    path('greg/', views.GregView.as_view(template_name='greg.html'), name='greg'),
-    path('steve/', views.SteveView.as_view(template_name='steve.html'), name='steve'),
+    path('bryan/', views.bryan, name='bryan'),
+    path('david/', views.david, name='david'),
+    path('greg/', views.greg, name='greg'),
+    path('steve/', views.steve, name='steve'),
 ]
 
 urlpatterns += [
--- birdie_app/views.py
+++ birdie_app/views.py
@@ -6,76 +6,59 @@ from django.views import generic
 # Create your views here.
 
 def home(request):
-    # context = {} 
-    # context['form'] = GeeksForm() 
-    # return render( request, "home.html", context)
-
-    # from .forms import GeeksForm 
-  
-    # # Create your views here. 
-    # def home_view(request): 
-    #     context ={} 
-    #     form = GeeksForm() 
-    #     context['form']= form 
-    #     if request.GET: 
-    #         temp = request.GET['geeks_field'] 
-    #         print(temp) 
-    #     return render(request, "home.html", context)
-
     if request.method == 'POST':
         form = BirdieForm(request.POST)
         if form.is_valid(): 
             form.save()
             birdies = Birdie.objects.all()
             return render(request, 'data.html', {'birdies': birdies})
-    # else:
-    #     form_class = BirdieForm
-    return render(request, 'home.html', {})
+    else:
+        form_class = BirdieForm
+    return render(request, 'home.html', {'form': form_class,})
 
 def data(request):
-    # if form submitted:
-        player_input = request.POST.get('player')
-        date_input = request.POST.get('date')
-        course_input = request.POST.get('course')
-        hole_input = request.POST.get('hole')
-        Birdie.objects.create(player=player_input, date=date_input, course=course_input, hole=hole_input)
-        stats = Birdie.objects.all()
-        # context = {'data': stats}
-        context = {'stats': stats}
-        return render(request, 'data.html', context)
+    # # creating new object or instance in the database
+    player_input = request.POST.get('player')
+    # print(player_input, '38')
+    date_input = request.POST.get('date')
+    course_input = request.POST.get('course')
+    hole_input = request.POST.get('hole')
+    Birdie.objects.create(player=player_input, date=date_input, course=course_input, hole=hole_input)
+    stats = Birdie.objects.all()
+    # context = {'data': stats}
+    context = {'stats': stats}
+    return render(request, 'data.html', context)
+    # return render(request, f'{player_input}.html', context)
     # else:
     #     stats = Birdie.objects.all()
     #     # context = {'data': stats}
     #     context = {'stats': stats}
     #     return render(request, 'data.html', context)
 
-class BryanView(generic.TemplateView):
-    # model = Birdie
-    # form_class = 
-    template_name = 'bryan.html'
-    # success_url = reverse_lazy('home')
+def bryan(request):
+    stats = Birdie.objects.filter(player='Bryan')
+    # context = {'data': stats}
+    context = {'stats': stats}
+    # return render(request, f'{player_input}.html', context)
+    return render(request, 'bryan.html', context)
 
-class DavidView(generic.TemplateView):
-    template_name = 'david.html'
+def david(request):
+    stats = Birdie.objects.filter(player='David')
+    # context = {'data': stats}
+    context = {'stats': stats}
+    # return render(request, f'{player_input}.html', context)
+    return render(request, 'david.html', context)
 
-class GregView(generic.TemplateView):
-    template_name = 'greg.html'
+def greg(request):
+    stats = Birdie.objects.filter(player='Greg')
+    # context = {'data': stats}
+    context = {'stats': stats}
+    # return render(request, f'{player_input}.html', context)
+    return render(request, 'greg.html', context)
 
-class SteveView(generic.TemplateView):
-    template_name = 'steve.html'
-
-# def get_name(request):
-#     # if this is a POST request we need to process the form data
-#     if request.method == 'POST':
-#         # create a form instance and populate it with data from the request:
-#         form = NameForm(request.POST)
-#         # check whether it's valid:
-#         if form.is_valid():
-#             # process the data in form.cleaned_data as required
-#             # ...
-#             # redirect to a new URL:
-#             return HttpResponseRedirect('/thanks/')
-#     # if a GET (or any other method) we'll create a blank form
-#     else:
-#         form = NameForm()
-#     return render(request, 'name.html', {'form': form})
+def steve(request):
+    stats = Birdie.objects.filter(player='Steve')
+    # context = {'data': stats}
+    context = {'stats': stats}
+    # return render(request, f'{player_input}.html', context)
+    return render(request, 'steve.html', context)
