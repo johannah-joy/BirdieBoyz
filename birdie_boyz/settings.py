2264e30e6e436e45284534196ae865316353b232
JoHannah Joy <jojojoy2012@gmail.com>
2023-04-30 16:28:40 -0700

    update app name on Data page

--- birdie_boyz/settings.py
+++ birdie_boyz/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '=j+t(@z0b1!xxp3gbhz6sabhof6=fjtungod%+vblns!2!#+in'
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'birdieboyz.herokuapp.com']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'birdieboyz.herokuapp.com', web-production-0e938.up.railway.app]
 ALLOWED_HOSTS = ['*']
 
 
